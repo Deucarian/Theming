1f16f2c784fd87c8a1ef9e25cb3c7140b51dfbd7
jhoevenberg <j.hoevenberg@simultria.nl>
2026-07-15 18:47:51 +0200

    Restore safe live theme previews

---
+++ Runtime/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Deucarian.Theming.Editor")]
+[assembly: InternalsVisibleTo("Deucarian.Theming.Tests")]
+[assembly: InternalsVisibleTo("Deucarian.Theming.Editor.Tests")]
